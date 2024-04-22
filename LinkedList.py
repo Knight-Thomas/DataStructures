c921880cc8950a548c5680b732971b2e46fb239f
Knight-Thomas <thomasmilesknight@icloud.com>
2024-04-22 10:50:33 +0100

    finished appender and display method

--- LinkedList.py
+++ LinkedList.py
@@ -14,8 +14,28 @@ class LinkedList:
     def appender(self, data):
         '''method to add items to the list'''
         self.data = data
-        node = self.head
-        while self.next != None:
-            ''
-
+        newNode = Node(data)
+        current = self.head
+        #iterate over existing list until the tail nodde
+        while current.next != None:
+            #traverse through the list
+            current = current.next
+        current.next = newNode
+        #make the next node the new node
     
+    def lengthOf(self):
+        'method to get length of list'
+    
+    def display(self):
+        'method to display list'
+        myList = []
+        current = self.head
+        while current.next != None:
+            current = current.next
+            myList.append(current.data)
+        print(myList)
+
+listy = LinkedList()
+listy.appender(5)
+listy.appender(9)
+listy.display()
