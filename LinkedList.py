b4a6c5281c9ce63cf933140f01b6d457ed20dd5d
Knight-Thomas <thomasmilesknight@icloud.com>
2024-04-24 13:25:10 +0100

    Adding base for the extactor method to remove items from the list

--- LinkedList.py
+++ LinkedList.py
@@ -22,9 +22,36 @@ class LinkedList:
             current = current.next
         current.next = newNode
         #make the next node the new node
+
+    def extractor(self):
+        '''method to remove items within the list'''
+        try:
+            index = int(input('Enter the index of the item to remove: '))
+            #check if the index passed is within the range
+            if index > self.lengthOf() or index<0:
+                return None
+            elif index == 0:
+                self.head = self.head.next
+            else:
+                i=0
+                while i<index:
+                    previous = current_node
+                    current_node = current_node.next
+                    i+=1
+                previous.next = current_node.next
+                current_node.next = None
+        except ValueError:
+            print('Index must be a number')
+            return None
     
     def lengthOf(self):
         'method to get length of list'
+        current = self.head
+        nodesVisited = 0
+        while current.next != None:
+            nodesVisited += 1
+            current = current.next
+        print(nodesVisited)
     
     def display(self):
         'method to display list'
@@ -38,4 +65,8 @@ class LinkedList:
 listy = LinkedList()
 listy.appender(5)
 listy.appender(9)
+listy.appender(15)
+listy.appender(75)
 listy.display()
+listy.lengthOf()
+listy.extractor()
